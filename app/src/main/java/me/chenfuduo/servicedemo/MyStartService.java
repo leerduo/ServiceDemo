4076be8f90ee2f825ca606c2b7969799c2eb90ad
chenlouis <619189810@qq.com>
2015-06-15 10:33:11 +0800

    add notification

--- app/src/main/java/me/chenfuduo/servicedemo/MyStartService.java
+++ app/src/main/java/me/chenfuduo/servicedemo/MyStartService.java
@@ -1,5 +1,7 @@
 package me.chenfuduo.servicedemo;
 
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.os.*;
@@ -44,6 +46,14 @@ public class MyStartService extends Service {
         mServiceLooper = thread.getLooper();
         mServiceHandler = new ServiceHandler(mServiceLooper);
 
+
+        Notification notification = new Notification(R.mipmap.ic_launcher, "tick text",
+                System.currentTimeMillis());
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
+        notification.setLatestEventInfo(this, "notification_title",
+                "notification_message", pendingIntent);
+        startForeground(1, notification);
 
     }
 
