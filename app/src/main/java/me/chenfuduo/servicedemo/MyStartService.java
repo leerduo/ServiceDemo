9f1e49d487ee59fe5efc21732d90fdfc7e0bcbf9
chenlouis <619189810@qq.com>
2015-06-15 09:43:30 +0800

    MyStartService add the same work as MyIntentService

--- app/src/main/java/me/chenfuduo/servicedemo/MyStartService.java
+++ app/src/main/java/me/chenfuduo/servicedemo/MyStartService.java
@@ -2,12 +2,18 @@ package me.chenfuduo.servicedemo;
 
 import android.app.Service;
 import android.content.Intent;
-import android.os.IBinder;
+import android.os.*;
+import android.os.Process;
 import android.util.Log;
 
 public class MyStartService extends Service {
 
     public static final String TAG = "MyStartService";
+
+    private Looper mServiceLooper;
+
+
+    private ServiceHandler mServiceHandler;
 
     public MyStartService() {
         Log.e(TAG,"构造器--->" + "MyStartService");
@@ -25,6 +31,20 @@ public class MyStartService extends Service {
     public void onCreate() {
         super.onCreate();
         Log.e(TAG,"onCreate");
+
+        // Start up the thread running the service.  Note that we create a
+        // separate thread because the service normally runs in the process's
+        // main thread, which we don't want to block.  We also make it
+        // background priority so CPU-intensive work will not disrupt our UI.
+        HandlerThread thread = new HandlerThread("ServiceStartArguments",
+                Process.THREAD_PRIORITY_BACKGROUND);
+        thread.start();
+
+        // Get the HandlerThread's Looper and use it for our Handler
+        mServiceLooper = thread.getLooper();
+        mServiceHandler = new ServiceHandler(mServiceLooper);
+
+
     }
 
 
@@ -32,7 +52,15 @@ public class MyStartService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.e(TAG,"onStartCommand");
-        return super.onStartCommand(intent, flags, startId);
+        // For each start request, send a message to start a job and deliver the
+        // start ID so we know which request we're stopping when we finish the job
+        Message msg = mServiceHandler.obtainMessage();
+        msg.arg1 = startId;
+        mServiceHandler.sendMessage(msg);
+
+        // If we get killed, after returning from here, restart
+        return START_STICKY;
+
     }
 
     @Override
@@ -40,4 +68,33 @@ public class MyStartService extends Service {
         super.onDestroy();
         Log.e(TAG,"onDestroy");
     }
+
+    private final class ServiceHandler extends Handler{
+
+        public ServiceHandler(Looper looper) {
+            super(looper);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            // Normally we would do some work here, like download a file.
+            // For our sample, we just sleep for 5 seconds.
+            long endTime = System.currentTimeMillis() + 5*1000;
+            while (System.currentTimeMillis() < endTime) {
+                synchronized (this) {
+                    try {
+                        wait(endTime - System.currentTimeMillis());
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            // Stop the service using the startId, so that we don't stop
+            // the service in the middle of handling another job
+            stopSelf(msg.arg1);
+        }
+
+    }
+
+
 }
